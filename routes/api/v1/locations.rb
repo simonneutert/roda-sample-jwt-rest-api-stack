8c1ea88903b309f9fafcb5ebb854cd945b3c65c1
simonneutert <simonneutert@users.noreply.github.com>
2022-06-27 15:22:04 +0200

    return json

--- routes/api/v1/locations.rb
+++ routes/api/v1/locations.rb
@@ -4,7 +4,8 @@ class App
   hash_routes('/api/v1').on 'locations' do |r|
     r.get do
       locations = LocationRepository.new
-      locations.for_user(@current_user, 100)
+      results = locations.for_user(@current_user, 100)
+      { locations: results }
     end
 
     r.post do
