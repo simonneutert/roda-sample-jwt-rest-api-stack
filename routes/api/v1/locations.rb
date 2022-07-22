710d63957922517b7df367a890c3044d9e8d8344
simonneutert <simonneutert@users.noreply.github.com>
2022-07-22 17:01:30 +0200

    fixes locations endpoint reponse on post

--- routes/api/v1/locations.rb
+++ routes/api/v1/locations.rb
@@ -4,17 +4,18 @@ class App
   hash_routes('/api/v1').on 'locations' do |r|
     r.get do
       locations = LocationRepository.new
-      results = locations.for_user(@current_user, 100)
+      results = locations.for_user(@current_user)
       { locations: results }
     end
 
     r.post do
       locations = LocationRepository.new
-      persisting = locations.save!(@current_user, r.params)
-      unless persisting[:status] == :success
-        response.status = persisting[:status] || 500
+      result = locations.save!(@current_user, r.params)
+      unless result[:status] == :success
+        response.status = result[:status] || 500
         next
       end
+      result
     end
   end
 end
