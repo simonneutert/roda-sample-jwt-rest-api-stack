09bf209bcb8f10b66355b45985f9ce61997f8b23
simonneutert <simonneutert@users.noreply.github.com>
2022-07-22 16:59:25 +0200

    comments on http tests

--- httpie.zsh
+++ httpie.zsh
@@ -7,20 +7,26 @@ LOCALHOST_BEARER=$(http -f POST localhost:9292/api/v1/auth email=abc@test.test p
 echo "Current token: $LOCALHOST_BEARER" 
 sleep 1
 
-http -A bearer -a $LOCALHOST_BEARER localhost:9292/api/v1/items 
-sleep 2
+# 200
+http -A bearer -a $LOCALHOST_BEARER localhost:9292/api/v1/items
+sleep 1
 
+ # 200
 http -A bearer -a $LOCALHOST_BEARER localhost:9292/api/v1/items/1
-sleep 2
+sleep 1
 
+# 404
 http -A bearer -a $LOCALHOST_BEARER localhost:9292/api/v1/items/0
-sleep 2
+sleep 1
 
 # https://httpie.io/docs/cli/nested-json
+# 200
 http -A bearer -a $LOCALHOST_BEARER POST localhost:9292/api/v1/locations "locations[]:={\"latitude\": 55, \"longitude\": 10}"
-sleep 2
+sleep 1
 
+# 200
 http -A bearer -a $LOCALHOST_BEARER localhost:9292/api/v1/locations
-sleep 2
+sleep 1
 
+# 401
 http -A bearer -a "failing_token!$" POST localhost:9292/api/v1/locations "locations[]:={\"latitude\": 55, \"longitude\": 10}"
